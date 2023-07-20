497b301abcd794bb6513a62bc7012bef136f2129
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-07-20 17:20:28 -0500

    progress:

--- Program.cs
+++ Program.cs
@@ -3,11 +3,18 @@ namespace CatWorx.BadgeMaker
 {
   class Program
   {
-    static void Main(string[] args)
+    static void Main()
     {
-      string greeting = "Hello ";
-      greeting = greeting + "World";
-      Console.WriteLine($"greetings: {greeting}");
+      // How do you find the area of a square? Area = side * side
+      // double side = 3.14;
+      // double area = side * side;
+      // string greeting = "Hello ";
+      // greeting = greeting + "World";
+      // Console.WriteLine($"area: {area}");
+      // Console.WriteLine(area.GetType());
+      bool isCold = true;
+      Console.WriteLine(isCold ? "drink" : "add ice");  // output: drink
+      Console.WriteLine(!isCold ? "drink" : "add ice");  // output: add ice
     }
   }
 }
